ba45a35b33e4a65e4ce7a3d242c32ea8553a87d5
Erh-Li Shen <erh-li.shen@workday.com>
2016-09-18 15:30:58 -0700

    Create Framework

---
+++ nMessenger-iOS/nMessenger.h
@@ -0,0 +1,19 @@
+//
+//  nMessenger.h
+//  nMessenger
+//
+//  Created by Erh-Li Shen on 9/18/16.
+//  Copyright © 2016 Ebay Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for nMessenger.
+FOUNDATION_EXPORT double nMessengerVersionNumber;
+
+//! Project version string for nMessenger.
+FOUNDATION_EXPORT const unsigned char nMessengerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <nMessenger/PublicHeader.h>
+
+
